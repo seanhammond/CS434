8f85a457e7fb7d9b518b17ec4fbe450e866798a9
ShinySaphire <isons@onid.oregonstate.edu>
2015-05-24 13:11:52 -0700

    switched to a set instead of arraylist because sets don't allow duplicates.

--- assignment5/src/hw5/Kmeans.java
+++ assignment5/src/hw5/Kmeans.java
@@ -1,23 +1,26 @@
 package hw5;
 
 import java.util.Random;
+import java.util.HashSet;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Kmeans {
 	public double [][] data;
 	public int k;
-	//public double [][] clusters;
 	public double [] centroids;
-	public java.util.ArrayList<Integer>[] clusters;
+	public java.util.Set<Integer>[] clusters;
+	public Boolean convergence;
 	
 	@SuppressWarnings("unchecked")
 	public Kmeans(double [][] source, int num){
 		this.data = source;
 		this.k = num;
 		this.centroids = new double[this.k];
-		 java.util.ArrayList<Integer>[] arr = new java.util.ArrayList[this.k];
+		this.convergence = false;
+		 java.util.Set<Integer>[] arr = new java.util.HashSet[this.k];
 		 for(int m = 0; m < this.k; m++){
-			  arr[m] = new java.util.ArrayList<Integer>();		 
+			  arr[m] = new java.util.HashSet<Integer>();		 
 		  }
 		this.clusters = arr;
 		Random rand = new Random();
@@ -29,14 +32,37 @@ public class Kmeans {
 			startingCentroid(point, i);
 		}
 		
-		//assign to cluster
-		assignCluster();
+		while(this.convergence == false){
+			//assign to cluster
+			assignCluster();
+		
+			//save current centers 
+			double currentCenters[] = this.centroids;
+			//calculate new centers
+			for(int n = 0; n < this.k; n++){
+				clusterCentroid(this.clusters[n], n);
+			}
+			//check if centers have changed, meaning no moving of points.
+			int sameness = 0;
+			for(int n = 0; n < this.k; n++){
+				if(currentCenters[n] == this.centroids[n]){
+				sameness++;
+			}
+			}
+			if(sameness==this.k){
+				this.convergence = true;
+			}
+		}
 	}
 	
 	public void printCluster(){
+		
 		for(int n = 0; n < this.clusters.length; n++){
-			for(int j=0; j < this.clusters[n].size(); j++){
-				int instance = this.clusters[n].get(j);
+			Iterator<Integer> iterator = this.clusters[n].iterator();
+			while(iterator.hasNext()){	
+			//for(int j=0; j < this.clusters[n].size(); j++){
+				int instance = iterator.next();
+				//int instance = this.clusters[n].get(j);
 				System.out.printf("Instance %d -> Cluster %d\n", instance, n);
 			}
 			
@@ -45,6 +71,7 @@ public class Kmeans {
 	}
 	
 	private void assignCluster(){
+		
 		for(int s=0; s < this.data.length; s++){
 			double temp = tempCentroid(s);
 			double best = 10000;
@@ -58,23 +85,16 @@ public class Kmeans {
 			}
 			this.clusters[clusterNum].add(s);
 		}
-		for(int n = 0; n < this.k; n++){
-			clusterCentroid(this.clusters[n], n);
-		}
+		
 	}
 	
 	//update centers for clusters
-	private void clusterCentroid(ArrayList<Integer> p, int clustNum){
+	private void clusterCentroid(java.util.Set<Integer> p, int clustNum){
 		double c=0;
-		System.out.printf("p size = %d\n", p.size());
-		System.out.printf("data size = %d\n", this.data[0].length);
-		double thing = data[1399][0];
-		double thing2 = data[1399][6];
-		System.out.printf("data[1399][0] = %f\n", thing);
-		System.out.printf("data[1399][6] = %f\n", thing2);
-		for(int j=0; j < p.size(); j++){
+		for(Integer ind : p){
 			for(int b = 0; b < this.data[0].length; b++){
-				int index = p.get(j);
+				int index = (Integer) ind;
+				System.out.printf("index b = %d\n", index);
 				c += this.data[index][b];
 			}
 		}
